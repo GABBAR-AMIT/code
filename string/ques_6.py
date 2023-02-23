314968ac0055ed92e8ca55daadbfcac4da154561
GABBAR-AMIT <amitsahu5323@gmail.com>
2023-02-23 20:30:14 +0530

    new

---
+++ string/ques_6.py
@@ -0,0 +1,18 @@
+# Write a Python program to add 'ing' at the end of a given string
+#(length should be at least 3). If the given string already ends with 'ing', add 'ly' instead.
+# If the string length of the given string is less than 3, leave it unchanged. 
+a=input("Enter a string:")
+if len(a)>=3:
+    if(a.endswith('ing')):
+        a+="ly"
+    else:
+        a+="ing"
+print(a)
+    
+        
+    
+
+
+
+
+ 
