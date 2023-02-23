86fa69050b041394c2c0b9653ac8922844b48845
GABBAR-AMIT <amitsahu5323@gmail.com>
2023-02-23 19:09:53 +0530

    new

--- string/ques_2.py
+++ string/ques_2.py
@@ -1,3 +1,14 @@
 # Write a Python program to count the number of characters
 # (character frequency) in a string. 
-a=(input)
+
+a="Google:"
+d={}
+for i in a:
+    k=d.keys()
+    if a in k: # comparison that is why using "k"
+        d[i]+=1 # data save that is why using "d"
+    else:
+        d[i]=1 # data save that is why using "d
+print(d)
+    
+    
