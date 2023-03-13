56b9e0d294a906b3e4b39e750d9207ee3c508b02
GABBAR-AMIT <amitsahu5323@gmail.com>
2023-03-13 23:37:04 +0530

    new

--- List/list_28.py
+++ List/list_28.py
@@ -1,1 +1,5 @@
-#
+#  Write a Python program to find the second largest number in a list
+n=[10,20,30,40,50,60,70,404,405]
+n.sort(reverse=True)
+second_largest_number=n[1]
+print(second_largest_number) 
