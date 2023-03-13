a85e4310f73f5e6207e7ac9a79402c77eccfef0c
GABBAR-AMIT <amitsahu5323@gmail.com>
2023-03-13 22:34:16 +0530

    new

---
+++ List/list_22.py
@@ -0,0 +1,11 @@
+# Write a Python program to find the index of an item in a specified list
+n=[2,4,6,7,8,9,99]
+a=n.index(9)
+print(a)
+
+
+a=(input("Entre the numbers :"))
+b=(input('enter the number to find index:'))
+i=a.index
+
+
