51b0e8a40c61f992375fec36d04d2b27a138245c
GABBAR-AMIT <amitsahu5323@gmail.com>
2023-03-16 09:17:47 +0530

    new

--- List/list_44.py
+++ List/list_44.py
@@ -1,11 +1,19 @@
 # Write a Python program to generate groups of five consecutive numbers in a list.
 list=[]
-for i in range(1,16):
+#l = []
+for i in range(1,11):
     if i % 5==1:
         l=[i]
+        print("if : ",l)
+        print("if list :", list)
     elif i % 5 !=0:
         l.append(i)
+        print("elif : ", l)
+        print("elif list :", list)
     else:
-        l.append(i)
+        l.append(i) # 1 is add from here 
+        print("else : ", l)
         list.append(l)
+        print("else list :", list)
 print(list)
+print(l)
