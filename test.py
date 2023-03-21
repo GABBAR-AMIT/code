37d5bdbabd8b99b1368d214d0bd347406d117218
GABBAR-AMIT <amitsahu5323@gmail.com>
2023-03-21 09:24:24 +0530

    new

--- test.py
+++ test.py
@@ -1,8 +1,3 @@
-a=[-1,-2,-3,4,5,6,7,8,9,10,11,12,13,14,15]
-b=[]
-for i in range(0,len(a),5):
-    c=[]
-    for j in range(i,i+5):
-        c.append(a[i])
-    b.append(c)
-print(b)
+a = open('test.txt','w')
+a.write('test file')
+a.close()
