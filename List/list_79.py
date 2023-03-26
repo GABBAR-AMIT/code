934940fec34940976da5fecb93a7e0a28316499e
GABBAR-AMIT <amitsahu5323@gmail.com>
2023-03-26 18:53:41 +0530

    new

---
+++ List/list_79.py
@@ -0,0 +1,2 @@
+# Write a Python program to remove the K'th element from a given 
+# list, and print the updated list
---
+++ List/list_82.py
@@ -0,0 +1,3 @@
+# Write a Python program to generate combinations of n 
+# distinct objects taken from the elements of a given list.
+from itertools import combinations
