423c99a432b3b09b24debd57dd0d915fcdce2814
GABBAR-AMIT <amitsahu5323@gmail.com>
2023-03-29 01:41:26 +0530

    new

---
+++ List/list_124.py
@@ -0,0 +1,3 @@
+# Write a Python program to find the maximum and minimum product of pairs of
+# tuples within a given list.
+
---
+++ List/list_126.py
@@ -0,0 +1,6 @@
+# Write a Python program to interleave multiple lists of the same length.
+list1=[1, 2, 3, 4, 5, 6, 7]
+list2=[10, 20, 30, 40, 50, 60, 70]
+list3=[100, 200, 300, 400, 500, 600, 700]
+a=[i for pair in zip(list1, list2,list3) for i in pair]
+print ("The list is : " + str(a))
