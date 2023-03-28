847ba23f1424348217c1594e348642056686132a
GABBAR-AMIT <amitsahu5323@gmail.com>
2023-03-28 20:33:49 +0530

    new

--- List/list_122.py
+++ List/list_122.py
@@ -1,1 +1,9 @@
-# Write a Python program to find common elements in a nested list. 
+# Write a Python program to find common elements in a nested list.
+def common(x):
+    b=set(x[0])
+    for i in x[1:]:
+        b.intersection_update(i)
+    return sorted(b)
+    
+a=[[12, 18, 23, 25, 45], [7, 12, 18, 24, 28], [1, 5, 8, 12, 15, 16, 18]]
+print(common(a))
