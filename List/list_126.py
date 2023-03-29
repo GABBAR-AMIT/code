df03d53f1c0bfb5d49a457802c4b9eb7a977f7a0
GABBAR-AMIT <amitsahu5323@gmail.com>
2023-03-29 20:24:58 +0530

    new

--- List/list_126.py
+++ List/list_126.py
@@ -1,6 +1,9 @@
 # Write a Python program to interleave multiple lists of the same length.
+def multiple_list(list1,list2,list3):
+    a=[i for j in zip(list1, list2,list3) for i in j]
+    return(a)
+
 list1=[1, 2, 3, 4, 5, 6, 7]
 list2=[10, 20, 30, 40, 50, 60, 70]
 list3=[100, 200, 300, 400, 500, 600, 700]
-a=[i for pair in zip(list1, list2,list3) for i in pair]
-print ("The list is : " + str(a))
+print (multiple_list(list1,list2,list3))
