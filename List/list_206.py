8a7ff248a7756ffa3801c5a974cb7a4bd8140dcf
GABBAR-AMIT <amitsahu5323@gmail.com>
2023-04-09 19:16:22 +0530

    new

---
+++ List/list_206.py
@@ -0,0 +1,5 @@
+# Write a Python program to remove additional spaces from a given list
+a=['abc ', ' ', ' ', 'sdfds ', ' ', ' ', 'sdfds ', 'huy']
+a=[ i.replace(' ','') for i in a]
+print(a)
+
---
+++ List/list_219.py
@@ -0,0 +1,3 @@
+# Write a Python program to build a list, using an iterator function and an initial seed value.
+def fun(seed):
+    result=seed 
