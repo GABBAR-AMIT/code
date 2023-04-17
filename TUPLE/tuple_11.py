83d1f06ede2cf35790621579645a47f65fba03d2
GABBAR-AMIT <amitsahu5323@gmail.com>
2023-04-17 10:03:09 +0530

    new

---
+++ TUPLE/tuple_11.py
@@ -0,0 +1,4 @@
+# Write a Python program to convert a list to a tuple
+a=[1,2,3,4,5,6,7,8,9]
+b=tuple(a)
+print(b)
---
+++ TUPLE/tuple_12.py
@@ -0,0 +1,6 @@
+#  Write a Python program to remove an item from a tuple
+fruits= ('apple', 'banana', 'cherry', 'orange')
+list=list(fruits)
+list.remove('apple')
+tuple=tuple(list)
+print(tuple)
