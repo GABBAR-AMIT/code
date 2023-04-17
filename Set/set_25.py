44f68b80d3ac868e1571c956f73ee923d50ca7a9
GABBAR-AMIT <amitsahu5323@gmail.com>
2023-04-17 19:57:34 +0530

    new

--- Set/set_25.py
+++ Set/set_25.py
@@ -1,4 +1,5 @@
-# Given two sets of numbers, write a Python program to find the missing numbers in the second set as compared to the first and vice versa. Use the Python set.
+# Given two sets of numbers, write a Python program to find the
+# missing numbers in the second set as compared to the first and vice versa. Use the Python set.
 def missing_numbers(set_nums1, set_nums2):
     return set(set_nums1) - set(set_nums2),set(set_nums2) - set(set_nums1) 
 
