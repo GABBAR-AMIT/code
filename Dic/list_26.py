7054ea7bf45707ed79ba1b929a86a33002321d16
GABBAR-AMIT <amitsahu5323@gmail.com>
2023-04-24 19:33:49 +0530

    new

---
+++ Dic/list_26.py
@@ -0,0 +1,6 @@
+# Write a Python program to count the values associated with a 
+# key in a dictionary.
+a=[{'id': 1, 'success': True, 'name': 'Lary'},
+ {'id': 2, 'success': False, 'name': 'Rabi'},
+ {'id': 3, 'success': True, 'name': 'Alex'}]
+print(sum(i['id'] for i in a))
