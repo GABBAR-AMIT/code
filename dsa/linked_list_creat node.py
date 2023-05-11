5763953b34cf7cf0c1f59ff882f4202d9689a128
GABBAR-AMIT <amitsahu5323@gmail.com>
2023-05-12 00:18:37 +0530

    new

---
+++ dsa/linked_list_creat node.py
@@ -0,0 +1,5 @@
+class node:
+    def __init__(self,data):
+        self.head=data
+        self.add=None
+    # node and class creating
---
+++ dsa/linked_list_list_create.py
@@ -0,0 +1,8 @@
+class node:
+    def __init__(self,data):
+        self.head = data
+        self.add=None
+        
+class l_list:
+    def __init__(self):
+        self.N1=None
