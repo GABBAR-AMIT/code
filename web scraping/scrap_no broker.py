52f3e748a28bf3e747b3a3b2213a23f767d2a8b6
Amit kumar sahu <amitsahu5323@gmail.com>
2023-06-07 00:36:49 +0530

    new

--- web scraping/scrap_no broker.py
+++ web scraping/scrap_no broker.py
@@ -1,10 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
-url="https://www.nobroker.in/"
+url="https://versus.com/en/nvidia-geforce-gtx-750-ti-vs-zotac-geforce-gtx-1050-mini"
 res=requests.get(url=url)
 #print(res._content)
 html_content=res.content
 soup=BeautifulSoup(html_content,'html.parser')
 print(soup.title.text)
 
-print(soup.find_all('ul'))
+for i in soup.find_all('div id'):
+    print(i.text)
