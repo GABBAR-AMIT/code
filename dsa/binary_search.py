ae57e773c7fdb499cd8744acde9ff4dd918f730f
Amit kumar sahu <amitsahu5323@gmail.com>
2023-06-11 15:35:07 +0530

    new

--- dsa/binary_search.py
+++ dsa/binary_search.py
@@ -3,24 +3,27 @@
 # in the sorted list. This makes it significantly faster than linear search, 
 # especially for large lists.
 
-def binary(arr,target):
-    left=0
-    right=len(arr)-1
+def binary_search(arr, target):
+    left = 0
+    right = len(arr) - 1
+
     while left <= right:
-        mid=(left+right)//2
-    
-    if arr[mid]==target:
-        return mid
-    
-    if arr[mid] < target:
-        left=mid+1
-    else:
-        right=mid-1
+        mid = (left + right) // 2
+
+        if arr[mid] == target:
+            return mid
+
+        if arr[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+
     return -1  # Return -1 if the target element is not found
+
 # Example usage:
 my_list = [1, 2, 4, 6, 7, 9]
 target_element = 6
-result = binary(my_list, target_element)
+result = binary_search(my_list, target_element)
 
 if result != -1:
     print(f"The target element {target_element} is found at index {result}.")
